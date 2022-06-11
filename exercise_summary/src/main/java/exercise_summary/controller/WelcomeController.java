c9c9d93f83835be37dd0b9e889b0d612292fce58
Yami An <yamiannephilim@gmail.com>
2022-06-11 16:10:23 +0700

    update

--- exercise_summary/src/main/java/exercise_summary/controller/WelcomeController.java
+++ exercise_summary/src/main/java/exercise_summary/controller/WelcomeController.java
@@ -1,11 +1,10 @@
 package exercise_summary.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 @WebServlet("/welcome")
